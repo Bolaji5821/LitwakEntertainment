09e2ef82c799d7682e8c7f5556e3e54dce1c526d
Justboj <oyedelebolaji84@gmail.com>
2025-05-12 10:39:49 +0100

    pg update

--- models/Event.js
+++ models/Event.js
@@ -1,70 +1,70 @@
-const { DataTypes } = require('sequelize');
+import { DataTypes } from 'sequelize';
 
-module.exports = (sequelize) => {
-  const Event = sequelize.define('Event', {
-    id: {
-      type: DataTypes.INTEGER,
-      primaryKey: true,
-      autoIncrement: true,
-    },
-    title: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    slug: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      unique: true,
-    },
-    description: {
-      type: DataTypes.TEXT,
-      allowNull: true,
-    },
-    imageUrl: {
-      type: DataTypes.STRING,
-      allowNull: true,
-    },
-    date: {
-      type: DataTypes.DATE,
-      allowNull: false,
-    },
-    time: {
-      type: DataTypes.STRING,
-      allowNull: true,
-    },
-    location: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    venue: {
-      type: DataTypes.STRING,
-      allowNull: true,
-    },
-    address: {
-      type: DataTypes.STRING,
-      allowNull: true,
-    },
-    ticketUrl: {
-      type: DataTypes.STRING,
-      allowNull: true,
-    },
-    isFeatured: {
-      type: DataTypes.BOOLEAN,
-      defaultValue: false,
-    },
-    userId: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      references: {
-        model: 'Users',
-        key: 'id',
-      },
-    },
-    isPublished: {
-      type: DataTypes.BOOLEAN,
-      defaultValue: false,
-    },
-  });
+export default (sequelize) => {
+    const Event = sequelize.define('Event', {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        title: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        slug: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+        },
+        description: {
+            type: DataTypes.TEXT,
+            allowNull: true,
+        },
+        imageUrl: {
+            type: DataTypes.STRING,
+            allowNull: true,
+        },
+        date: {
+            type: DataTypes.DATE,
+            allowNull: false,
+        },
+        time: {
+            type: DataTypes.STRING,
+            allowNull: true,
+        },
+        location: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        venue: {
+            type: DataTypes.STRING,
+            allowNull: true,
+        },
+        address: {
+            type: DataTypes.STRING,
+            allowNull: true,
+        },
+        ticketUrl: {
+            type: DataTypes.STRING,
+            allowNull: true,
+        },
+        isFeatured: {
+            type: DataTypes.BOOLEAN,
+            defaultValue: false,
+        },
+        userId: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            references: {
+                model: 'Users',
+                key: 'id',
+            },
+        },
+        isPublished: {
+            type: DataTypes.BOOLEAN,
+            defaultValue: false,
+        },
+    });
 
-  return Event;
+    return Event;
 };
--- models/Mixtape.js
+++ models/Mixtape.js
@@ -1,63 +1,63 @@
-const { DataTypes } = require('sequelize');
+import { DataTypes } from 'sequelize';
 
-module.exports = (sequelize) => {
-  const Mixtape = sequelize.define('Mixtape', {
-    id: {
-      type: DataTypes.INTEGER,
-      primaryKey: true,
-      autoIncrement: true,
-    },
-    title: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    slug: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      unique: true,
-    },
-    description: {
-      type: DataTypes.TEXT,
-      allowNull: true,
-    },
-    coverUrl: {
-      type: DataTypes.STRING,
-      allowNull: true,
-    },
-    artist: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    releaseDate: {
-      type: DataTypes.DATE,
-      allowNull: true,
-    },
-    duration: {
-      type: DataTypes.STRING,
-      allowNull: true,
-    },
-    tracksCount: {
-      type: DataTypes.INTEGER,
-      allowNull: true,
-      defaultValue: 0,
-    },
-    fileUrl: {
-      type: DataTypes.STRING,
-      allowNull: true,
-    },
-    userId: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      references: {
-        model: 'Users',
-        key: 'id',
-      },
-    },
-    isPublished: {
-      type: DataTypes.BOOLEAN,
-      defaultValue: false,
-    },
-  });
+export default (sequelize) => {
+    const Mixtape = sequelize.define('Mixtape', {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        title: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        slug: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+        },
+        description: {
+            type: DataTypes.TEXT,
+            allowNull: true,
+        },
+        coverUrl: {
+            type: DataTypes.STRING,
+            allowNull: true,
+        },
+        artist: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        releaseDate: {
+            type: DataTypes.DATE,
+            allowNull: true,
+        },
+        duration: {
+            type: DataTypes.STRING,
+            allowNull: true,
+        },
+        tracksCount: {
+            type: DataTypes.INTEGER,
+            allowNull: true,
+            defaultValue: 0,
+        },
+        fileUrl: {
+            type: DataTypes.STRING,
+            allowNull: true,
+        },
+        userId: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            references: {
+                model: 'Users',
+                key: 'id',
+            },
+        },
+        isPublished: {
+            type: DataTypes.BOOLEAN,
+            defaultValue: false,
+        },
+    });
 
-  return Mixtape;
+    return Mixtape;
 };
